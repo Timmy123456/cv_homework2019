4cad5476a275fc05625745e2e9fa7be725c7a0eb
Timmy Cheng <timmy_cheng@foxmail.com>
2019-06-16 11:22:27 +0800

    1.求出灰度图的最大最小值和均值; 2.对灰度图进行MINMAX归一化; 3.使用公式i`=2*(i-128)+128增加图片对比度; 4.对结果进行显示

--- image_transform/image_transform.py
+++ image_transform/image_transform.py
@@ -8,4 +8,26 @@ if __name__ == '__main__':
 
     #转换成灰度图
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    #获取最大值、最小值、均值并输出
+    gray_max = np.max(gray)
+    gray_min = np.min(gray)
+    gray_ave = np.average(gray)
+    print(gray_max)
+    print(gray_min)
+    print(gray_ave)
+
+    #尺度变换
+    gray_norm = np.zeros(gray.shape)
+    gray_norm = cv2.normalize(gray, None, 0, 255, cv2.NORM_MINMAX) #归一化到0~255
+
+    #改变对比度
+    gray_sharp = 2 * (gray_norm - 128) + 128
+
+    #效果显示
     print(gray)
+    print(gray_norm)
+    print(gray_sharp)
+    cv2.imshow("gray", gray)
+    cv2.imshow("norm", gray_norm)
+    cv2.imshow("sharp", gray_sharp)
+    cv2.waitKey(0)
