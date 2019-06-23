83dbcca1b8fb470098198ce849baa8180e906eaa
Timmy Cheng <timmy_cheng@foxmail.com>
2019-06-23 18:34:26 +0800

    1.添加保存图片 2.添加打印的信息

--- image_transform/image_transform.py
+++ image_transform/image_transform.py
@@ -12,22 +12,22 @@ if __name__ == '__main__':
     gray_max = np.max(gray)
     gray_min = np.min(gray)
     gray_ave = np.average(gray)
-    print(gray_max)
-    print(gray_min)
-    print(gray_ave)
+    print("max:", gray_max)
+    print("min:", gray_min)
+    print("average:", gray_ave)
 
     #尺度变换
     gray_norm = np.zeros(gray.shape)
     gray_norm = cv2.normalize(gray, None, 0, 255, cv2.NORM_MINMAX) #归一化到0~255
 
     #改变对比度
-    gray_sharp = 2 * (gray_norm - 128) + 128
+    gray_transform = 2 * (gray_norm - 128) + 128
 
     #效果显示
-    print(gray)
-    print(gray_norm)
-    print(gray_sharp)
     cv2.imshow("gray", gray)
     cv2.imshow("norm", gray_norm)
-    cv2.imshow("sharp", gray_sharp)
+    cv2.imshow("transform", gray_transform)
     cv2.waitKey(0)
+    cv2.imwrite("./gray.png", gray)
+    cv2.imwrite("./gray_norm.png", gray_norm)
+    cv2.imwrite("./gray_transform.png", gray_transform)
